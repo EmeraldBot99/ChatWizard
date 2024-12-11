958cc2c50252204013c35302bb2ecc72ff3e9caa
EmeraldBot99 <118036239+EmeraldBot99@users.noreply.github.com>
2024-12-10 21:42:47 -0700

    basic layout

---
+++ server/src/crypto/KeyPair.rs
@@ -0,0 +1,17 @@
+use rand;
+
+pub struct KeyPair{
+    private_key: Vec<u8>,
+    public_key: Vec<u8>,
+
+
+}    
+impl KeyPair {
+    pub fn generate() -> Self{
+
+    }
+    pub fn derive_shared_secret() -> Vec<u8>{
+
+    }
+}
+
